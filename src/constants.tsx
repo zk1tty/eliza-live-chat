90578b0cbaeb31702f80198306204c7c4c361698
moon <shawmakesmagic@gmail.com>
2024-10-11 02:00:50 -0700

    more hot garb

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,6 +1,6 @@
 export const oai =
-    import.meta.env.VITE_OPENAI_API_KEY ?? ('sk-' + 'IJIqeGXk0LyYkBIAPFkET3BlbkFJWBZSrG2KMgApy3xUi9uH')
-export const sk = import.meta.env?.VITE_SIMLI_API_KEY ?? 
+    import.meta.env.VITE_OPENAI_API_KEY ?? 'sk-' + 'IJIqeGXk0LyYkBIAPFkET3BlbkFJWBZSrG2KMgApy3xUi9uH'
+export const sk = import.meta.env?.VITE_SIMLI_API_KEY ?? 'dlqhn6n14udwl4z3v66pn'
 export const e11 =
     import.meta.env?.VITE_ELEVENLABS_API_KEY ??
-    ('sk_' + 'db6b7ebae17ee002ca26b741b55ae505545f5235149999f4')
+    'sk_' + 'db6b7ebae17ee002ca26b741b55ae505545f5235149999f4'
