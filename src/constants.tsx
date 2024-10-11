87e3da8b33f66179eb259eeaef2d9ec77726df64
moon <shawmakesmagic@gmail.com>
2024-10-11 01:31:45 -0700

    keys

---
+++ src/constants.tsx
@@ -0,0 +1,3 @@
+export const oai = import.meta.env.VITE_OPENAI_API_KEY
+export const sk = import.meta.env.VITE_SIMLI_API_KEY
+export const e11 = import.meta.env.VITE_ELEVENLABS_API_KEY
--- src/k.tsx
+++
@@ -1,3 +0,0 @@
-export const oai = 'sk-' + 'IJIqeGXk0LyYkBIAPFkET3BlbkFJWBZSrG2KMgApy' + '3xUi9uH'
-export const sk = 'dlqhn6n' + '14udwl4z3v66pn'
-export const e11 = 'sk_' + 'db6b7ebae17ee002ca26b74' + '1b55ae505545f5235149999f4'
